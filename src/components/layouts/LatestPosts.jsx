c2963ea1fc2b1a053a11a9fa480bdbb126f28e56
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-01-25 05:32:11 +0000

    style: changed the styling of the recent blog posts cards

--- src/components/layouts/LatestPosts.jsx
+++ src/components/layouts/LatestPosts.jsx
@@ -7,16 +7,22 @@ function LatestPosts() {
     {
       id:1,
       title:'How I changed my portfolio site from HTML/CSS/ JS to my first React/Tailwind/Hashnode project [Part 1 -Intro/Design]',
-      date: '12 Feb 2022',
+      coverImage: 'https://source.unsplash.com/random/1610x850',
+      dateAdded: '11 Jan 2022',
       tags:'HTML, CSS, JS',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      slug: '/article1',
+      reactionCount: 27
     },
     {
       id:2,
       title:'Udemy Course Review - React JS Course',
-      date: '14 Feb 2022',
-      tags:'HTML, CSS, JS',
+      coverImage: 'https://source.unsplash.com/random/1600x840',
+      dateAdded: '14 Feb 2022',
+      tags:'React JS, Portfolio, Hashnode',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.', 
+      slug: '/article2',
+      reactionCount: 43
     },
   ]
 
@@ -26,24 +32,52 @@ function LatestPosts() {
         <div className="contents lg:text-center">
         <p className="grid text-sky-400 place-content-end text-ss hover:text-sky-700">View All</p>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Check Out My Recent Blog Posts
+            Check Out My Most Recent Blog Posts
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-            accusamus quisquam.
+            Check out my two most recent blogs below. but do not forget to check out all 25 that I have written.
           </p>
         </div>
           {/* TODO- fix the mobile responsivness */}
         <div className="mt-10">
           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
             {blogs.map((blog) => (
-              <div key={blog.id} className="relative bg-white hover:bg-slate-100 h-48 grid content-center justify-center">
-                <dt>
-                  <p className="mt-4 ml-4 text-lg leading-6 font-medium text-gray-900">{blog.title}</p>
-                </dt>
-                <dd className="m-4 text-s text-gray-500">{blog.date} | {blog.tags}</dd>
-                <dd className="m-4 text-s text-gray-500">{blog.description}</dd>
-              </div>
+                <div key={blog.id} >
+                  <img
+                    src={blog.coverImage}
+                    alt=" random imgee"
+                    className="w-full object-cover object-center rounded-lg shadow-md"
+                  />
+
+                  <div className="relative px-4 -mt-16">
+                    <div className="bg-white p-6 rounded-lg shadow-lg">
+                      <div className="flex items-baseline">
+                        <span
+                          className="bg-sky-200 text-sky-800 text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide"
+                        >
+                          Recent Post
+                        </span>
+                        <div
+                          className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider"
+                        >
+                          {blog.tags}
+                        </div>
+                      </div>
+
+                      <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">
+                      {blog.title}
+                      </h4>
+
+                      <div className="mt-1">
+                      {blog.description}
+                      </div>
+                      <div class="mt-4">
+                        <span className="text-sky-600 text-md font-semibold">{blog.dateAdded} </span>
+                        <span className="text-sm text-gray-600">(currently with {blog.reactionCount} reactions)</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             ))}
           </dl>
         </div>
