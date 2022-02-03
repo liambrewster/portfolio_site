7904deeaa8268d75c3f3bc20c82b43634e5b122a
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-02-03 00:08:51 +0000

    fix: #30 make latest post mobile responsive

--- src/components/layouts/LatestPosts.jsx
+++ src/components/layouts/LatestPosts.jsx
@@ -29,16 +29,15 @@ function LatestPosts() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="contents lg:text-center">
         <p className="grid text-sky-400 place-content-end text-ss hover:text-sky-700"> <a href="https://blog.liambrewster.co.uk">View All</a></p>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-2 text-xl md:text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             Check Out My Most Recent Blog Posts
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          <p className="mt-4 max-w-2xl text-base md:text-xl text-gray-500 lg:mx-auto">
             Check out my two most recent blogs below. but do not forget to check out all of them.
           </p>
         </div>
-          {/* TODO- fix the mobile responsivness */}
         <div className="mt-10">
-          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+          <dl className="space-y-6 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
             {blogs.map((blog) => (
                 <div key={blog.id} >
                   <img
@@ -51,27 +50,27 @@ function LatestPosts() {
                     <div className="bg-white p-6 rounded-lg shadow-lg">
                       <div className="flex items-baseline">
                         <span
-                          className="bg-sky-200 text-sky-800 text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide"
+                          className="bg-sky-200 text-sky-800 text-xs px-2 truncate inline-block rounded-full uppercase font-semibold tracking-tighter md:tracking-wider "
                         >
                           Recent Post
                         </span>
                         <div
-                          className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider"
+                          className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-tighter md:tracking-wider truncate "
                         >
                           {blog.tags}
                         </div>
                       </div>
 
-                      <a href={blog.slug}><h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">
+                      <a href={blog.slug}><h4 className="mt-1 text-base md:text-xl font-semibold uppercase leading-tight truncate">
                       {blog.title}
                       </h4></a>
 
-                      <div className="mt-1">
+                      <div className="mt-1 text-xs md:text-base truncate md:whitespace-normal ">
                       {blog.description}
                       </div>
                       <div class="mt-4">
                         <span className="text-sky-600 text-md font-semibold">{blog.dateAdded} </span>
-                        <span className="text-sm text-gray-600">(currently with {blog.reactionCount} reactions)</span>
+                        <span className="text-sm text-gray-600 invisible md:visible">(currently with {blog.reactionCount} reactions)</span>
                       </div>
                     </div>
                   </div>
