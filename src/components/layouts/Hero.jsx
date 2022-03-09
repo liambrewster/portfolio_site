ae94c084b0e593ba052a5966640e8994f061711c
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-03-09 21:06:19 +0000

    update: updated the hero text wording

--- src/components/layouts/Hero.jsx
+++ src/components/layouts/Hero.jsx
@@ -8,22 +8,22 @@ function Hero() {
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
+              <p className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
                 <span className="block xl:inline">Hi, I am </span>
                 <span className="block text-sky-500 xl:inline">Liam Brewster</span>
-              </h1>
+              </p>
 
-              
+              <h1 className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0'>A Sheffield based, Website designer, Developer &amp; Social Media Manager / Marketer</h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-              A UK based, Website designer &amp; developer, who wants to give your customers the most user-friendly and functional website, Helping you perform better and grow online. I am looking forward to collaborate with you on your amazing website, so let's chat.
+              I want your customers to see the best side of you by ensuring you have the most user-friendly and functional online prescence through the use of websites or social media, We help you perform better and grow online. I am looking forward to collaborate with you on your amazing website to thrive online, so let's chat.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="#featured"
+                    href="#contactform"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-sky-400 hover:bg-sky-700 md:py-4 md:text-lg md:px-10"
                   >
-                    Check Out My Work
+                    Send Me A Message
                   </a>
                 </div>
               </div>
