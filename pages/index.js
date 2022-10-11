a9ddf15108596321c01d3805e860a59274951ba7
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-10-11 22:36:45 +0100

    feat: initial next rebuild commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import ContainerBlock from '../components/ContainerBlock';
+import FavouriteProjects from '../components/FavouriteProjects';
+import LatestCode from '../components/LatestCode';
+import Hero from '../components/Hero';
+import getLatestRepos from '@lib/getLatestRepos';
+import userData from '@constants/data';
+
+export default function Home({ repositories }) {
+	return (
+		<ContainerBlock
+			title='Liam Brewster - Developer, Designer, Consultant, Marketeer'
+			description='Need a website designed for your business? An interesting web developement project? Maybe a small update? Let us focus on creating your website that performs well in search engine rankings.'
+		>
+			<Hero />
+			<FavouriteProjects />
+			<LatestCode repositories={repositories} />
+		</ContainerBlock>
+	);
+}
+
+export const getServerSideProps = async () => {
+	console.log(process.env.GITHUB_AUTH_TOKEN);
+	let token = process.env.GITHUB_AUTH_TOKEN;
+
+	const repositories = await getLatestRepos(userData, token);
+	console.log('REPOSITORIES', repositories);
+
+	return {
+		props: {
+			repositories,
+		},
+	};
+};
